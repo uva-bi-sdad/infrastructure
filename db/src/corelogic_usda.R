9a3e96ca8813b4656a11e8aa2ee467c6674cd943
Aaron D. Schroeder <ads7fg@virginia.edu>
2020-02-26 20:53:59 +0000

    corelogics

--- db/src/corelogic_usda.R
+++ db/src/corelogic_usda.R
@@ -18,6 +18,15 @@ for (y in c("2005","2006","2007","2008","2009","2010","2011","2012","2013","2014
 
 "\copy usda_deed_2015 from /project/biocomplexity/sdad/projects-active/usda/bb/original/corelogic/corelogic_usda/deed/corelogic-USDA_Deed_2015.csv WITH (FORMAT 'csv', DELIMITER E'\|', QUOTE E'\"', HEADER);"
 
+# select columns
+'cut -d "|" -f 1-145 UVA_300000123123966_TaxHist01_Delivery_20190827.txt > ../../../UVA_300000123123966_TaxHist01_Delivery_20190827_145cols.txt'
+# remove non-ascii
+"tr -cd '\11\12\15\40-\176' < UVA_300000123123966_TaxHist01_Delivery_20190827_145cols.txt > UVA_300000123123966_TaxHist01_Delivery_20190827_145cols_ascii.txt"
+# validate number of delimiters
+"tr -cd '|\n' < UVA_300000123123966_TaxHist01_Delivery_20190827_145cols_ascii.txt | awk 'length!=144 {print NR}' > wrong_number_pipes.txt"
+# upload NO QUOTE
+"\copy corelogic_sdad.tax_hist_01 from /project/biocomplexity/sdad/projects_data/UVA_300000123123966_TaxHist01_Delivery_20190827_145cols_ascii.txt WITH (FORMAT 'csv', DELIMITER E'\|', QUOTE E'\b', HEADER);"
+
 
 tax_cols <- fread("/project/biocomplexity/sdad/projects-active/usda/bb/original/corelogic/corelogic_usda/tax/TaxHistory.Tax_History_cols.txt") %>% 
   colnames() %>% 
@@ -63,4 +72,47 @@ DBI::dbDisconnect(con)
 "\copy state8_tax from /project/biocomplexity/sdad/projects-active/usda/bb/original/corelogic/8state/8State_Tax_10.txt WITH (FORMAT 'csv', DELIMITER E'\|', QUOTE '`', HEADER);"
 
 
-"CREATE INDEX corelogic_usda_2006_fips_trgm_idx ON corelogic_usda.usda_deed_2006 USING GIST (fips gist_trgm_ops);"
+sql <- paste0("CREATE INDEX corelogic_sdad_tax_hist_1", f,"_fips_trgm_idx ON corelogic_usda.usda_deed_2006 USING GIST (fips gist_trgm_ops);")
+
+
+
+
+library("maps")
+library("stringr")
+
+state_fips <- state.fips$fips %>% unique() %>% 
+  str_pad(2, pad = "0")
+
+for (f in state_fips[6:49]) {
+  sql <- paste0("select * into corelogic_sdad.tax_hist_2_", f," from corelogic_sdad.tax_hist_2 where fips_code like '", f, "%';")
+  con <- get_db_conn()
+  dbExecute(con, sql)
+  dbDisconnect(con)
+}
+
+
+
+tax_hist_01_cols <- fread("/project/biocomplexity/sdad/projects_data/volume_nyc1_03/Biocomplexity-CoreLogic/05092019_Corelogic_Data_Delivery/UVA_300000123123966_TaxHist01_Delivery_20190827.txt", nrows = 1) %>% colnames() %>% 
+  gsub("\\`", "", .) %>%
+  make.names() %>% 
+  gsub("\\.", "_", .) %>% 
+  tolower() %>% 
+  paste0("\"", ., "\"", " varchar")
+
+
+tax_hist_01_cols <- tax_hist_01_cols[1:(length(tax_hist_01_cols)-3)]
+tax_hist_01_cols_create <- paste0("CREATE TABLE tax_hist_01 (", paste(TaxHist01_cols, collapse = ","),")")
+
+for (i in c("01","02","03","04","05","06","07","08","09","10")) {
+  sql <- paste0("CREATE TABLE corelogic_sdad.tax_hist_", i, " (", paste(tax_hist_01_cols, collapse = ","),")")
+  con <- get_db_conn()
+  dbExecute(con, sql)
+  dbDisconnect(con)
+}
+
+print(Sys.time())
+reaadtest <- fread("/project/biocomplexity/sdad/projects_data/volume_nyc1_03/Biocomplexity-CoreLogic/05092019_Corelogic_Data_Delivery/UVA_300000123123966_TaxHist01_Delivery_20190827.txt")
+print(Sys.time())
+
+
+
--- db/src/db_connection.R
+++ db/src/db_connection.R
@@ -2,7 +2,7 @@ get_db_conn <-
   function(db_name = "sdad",
            db_host = "postgis1",
            db_port = "5432",
-           db_user = Sys.getenv("db_userid"),
+           db_user = Sys.getenv("db_usr"),
            db_pass = Sys.getenv("db_pwd")) {
     RPostgreSQL::dbConnect(
       drv = RPostgreSQL::PostgreSQL(),
